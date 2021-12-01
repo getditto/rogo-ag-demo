2de12382ef9ef955d1aeeafab583fde3d817bfc3
Maximilian Alexander <max@ditto.live>
2021-12-01 12:00:57 -0800

    adding readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Local React App Small Peer to NodeJS Small Peer
+
+* The React App is in the `./web` directory
+* The NodeJS small peer is in the `./server` directory
+
+## Setting up
+
+1. Make sure you have Node 16 or higher (this isn't necessary for Ditto, it is just easier for this project)
+2. Run `npm install` or `yarn` to install the packages in `./web` and `./server`
+3. Run `npm run dev` or `yarn dev` in  `./web` and `./server`
+4. The web app will start on http://localhost:3000, navigate to this in your browser. 
+5. The server app will start on http:localhost:4000
+
+Experiment with the code and give it a try!
--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Ditto } from "@dittolive/ditto"
+import { Ditto, TransportConfig } from "@dittolive/ditto"
 import { DittoProvider } from "@dittolive/react-ditto";
 import MainScene from "./MainScene";
 
@@ -10,7 +10,16 @@ function App() {
         type: 'onlinePlayground',
         appID: 'ce914163-2df7-4985-9821-7034891cdcda',
         enableDittoCloudSync: true
-      }, 'ditto')
+      }, 'ditto-data')
+      
+      
+      const config = new TransportConfig();
+      config.enableDittoCloudSync = true;
+      config.connect.websocketURLs.push("ws://0.0.0.0:4000")
+      
+      ditto.setTransportConfig(config);
+      
+      
       ditto.setLicenseToken('o2d1c2VyX2lkeCRmZWNlYTI1Ny04MWQ5LTQxMWMtYWZhMy1mYmQ1Nzk1NTQ0ZGZmZXhwaXJ5eBgyMDIyLTAzLTMxVDIxOjU5OjU5Ljg5NFppc2lnbmF0dXJleFhDV2laU2tzME9MSjF6clR6N1NpR0hNUjQ4WExvY3VxNHBvMTVIY3VKTmxJeEhSdFEyR3BkcjlZZG5JVXFFeFFRNGMvSkJGa1owNEFXZjJQN3diY1B2UT09')
       ditto.tryStartSync();
       return ditto
